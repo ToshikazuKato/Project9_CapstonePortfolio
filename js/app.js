c7addb63e17c61d53e0ce2bfa1a7b300f8ab7302
ToshikazuKato <kazu1427bsk@gmail.com>
2018-09-20 00:48:11 +0530

    Work section and organize css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-const screenWidth = window.innerWidth;
 const bar = document.getElementById('bar');
-const links = document.querySelectorAll('.links');
 const toggleNav = document.getElementById('toggleNav');
 
 // to display links when bar's clicked;
@@ -15,6 +13,37 @@ bar.addEventListener('click', ()=>{
   toggleLinks();
 });
 
+//draw svg
+const header = $('.header'),
+      win = $(window),
+      svg = $('#Contours').drawsvg({
+              duration:2000,
+              stagger:100,
+              easing:'swing',
+              reverse:false,
+            }),
+      max = header.height() - win.height();
+
+win.on('scroll', function(event){
+  event.preventDefault();
+  var  p = win.scrollTop()/max;
+  console.log(p);
+  svg.drawsvg('progress',p);
+});
+
+
+//slide effect
 AOS.init({
   duration: 1200,
-})
+});
+
+// smooth scroll when link is clicked
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
